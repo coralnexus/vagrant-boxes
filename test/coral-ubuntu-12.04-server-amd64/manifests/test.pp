bb335258711f4a05b8a5df90263db3443719c534
Adrian Webb <adrian.webb@coraltech.net>
2013-06-18 04:37:01 -0400

    Adjusting the box url in the Vagrantfile and simplifying the puppet snippet for testing.

--- test/coral-ubuntu-12.04-server-amd64/manifests/test.pp
+++ test/coral-ubuntu-12.04-server-amd64/manifests/test.pp
@@ -1,10 +1,6 @@
-group { "puppet":
-  ensure => "present",
-}
-
-File { owner => 0, group => 0, mode => 0644 }
 
 file { '/etc/motd':
   content => "Welcome to your Vagrant-built virtual machine!
-              Managed by Puppet.\n"
+              Managed by Puppet.\n",
+  mode => '0644'
 }
