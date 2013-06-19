bc7f362e424ed59d7f82310c844aeeca29aa9ece
Adrian Webb <adrian.webb@coraltech.net>
2013-06-18 20:19:09 -0400

    Updating the puppet.sh postinstall script to utilize the Puppetlabs apt repository and including a default Hiera config for Puppet.

--- definitions/coral-ubuntu-12.04-server-amd64/puppet.sh
+++ definitions/coral-ubuntu-12.04-server-amd64/puppet.sh
@@ -1,3 +1,33 @@
 
+# Set up Puppet Apt repositories
+apt-key adv --recv-key --keyserver pgp.mit.edu 4BD6EC30
+
+( cat <<'EOP'
+deb http://apt.puppetlabs.com precise main dependencies
+deb-src http://apt.puppetlabs.com precise main dependencies
+EOP
+) > /etc/apt/sources.list.d/puppetlabs.list
+chmod 0644 /etc/apt/sources.list.d/puppetlabs.list
+
 # Install Puppet
+apt-get -y update
 apt-get -y install puppet
+
+# Set up Hiera configuration
+mkdir /var/hiera
+
+( cat <<'EOP'
+---
+:merge_behavior: deeper
+:backends:
+  - yaml
+  - json
+:yaml:
+  :datadir: /var/hiera
+:json:
+  :datadir: /var/hiera
+:hierarchy:
+  - common
+EOP
+) > /etc/puppet/hiera.yaml
+chmod 0440 /etc/puppet/hiera.yaml
