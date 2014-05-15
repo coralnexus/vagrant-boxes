34f6b43db509a07f01f6e379119c80a3bea630f8
Adrian Webb <adrian.webb@coralnexus.com>
2014-05-15 18:57:40 -0400

    Changing the apt upgrade to dist-upgrade in the base Ubuntu install script.

--- scripts/ubuntu/base.sh
+++ scripts/ubuntu/base.sh
@@ -2,7 +2,7 @@
 # Apt-install various things necessary for Ruby, guest additions,
 # etc., and remove optional things to trim down the machine.
 apt-get -y update
-apt-get -y upgrade
+apt-get -y dist-upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev dkms
 apt-get -y install vim
