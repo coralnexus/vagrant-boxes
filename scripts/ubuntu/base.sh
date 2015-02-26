4e2dec5751ec63b0289a4d2bcea0ec09d5330021
Adrian Webb <adrian.webb@coraltech.net>
2015-02-26 01:34:51 -0500

    Fixing issues and cleaning up the Ubuntu 12.04 and 14.04 base box definitions.

--- scripts/ubuntu/base.sh
+++ scripts/ubuntu/base.sh
@@ -1,18 +1,21 @@
 
 # Apt-install various things necessary for Ruby, guest additions,
 # etc., and remove optional things to trim down the machine.
+rm -rf /var/lib/apt/lists/*
 apt-get -y update
-apt-get -y dist-upgrade
+apt-get -y upgrade
+
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev dkms
 apt-get -y install vim
-apt-get clean
 
-# Install base utilities
-apt-get -y install wget curl
+apt-get clean
 
 # Install NFS client
 apt-get -y install nfs-common nfs-kernel-server bindfs
+
+# Install base utilities
+apt-get -y install wget curl
 
 # Install Rsync
 apt-get -y install rsync
